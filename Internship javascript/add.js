5da88c16db73cf57e480b43b0b2e88b49c59affe
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-02-05 19:07:14 +0100

    ..

---
+++ Internship javascript/add.js
@@ -0,0 +1,9 @@
+function add(a, b){
+    return a + b;
+}
+
+var val = add(5, 7)
+var vall = add(10,8)
+console.log(val);
+console.log(vall)
+
---
+++ Internship javascript/file%3A/C%3A/Users/Dell/Desktop/Training/Javascript/Internship%2520javascript/add.js
@@ -0,0 +1,7 @@
+function add(a, b){
+    return a + b;
+}
+
+var val = add(7, 7)
+console.log(val);
+
