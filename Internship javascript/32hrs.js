17aae13ef5d5ab694ef6bbd2badf2eb6b7a84a42
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-03-01 19:03:59 +0100

    Objects (#27)
    * updated
    
    * feature: learning about objects
    
    * data structure
    
    * data structure
    
    * updated
    
    * updated
    
    * updated
    
    * first commit
    
    ---------
    
    Co-authored-by: Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>

---
+++ Internship javascript/32hrs.js
@@ -0,0 +1,28 @@
+//ways of creating arr
+var color1 = new Array("red", "blue", "green")
+console.log(color1[0])
+var color2 = ["red", "blue", "green"]
+console.log(color2[2])
+var animals = ["cat", "dog", "horse"]
+animals.push("cattle")
+console.log(animals);
+animals.splice(1, 1)
+animals.sort();
+console.log(animals);
+
+var result = 0;
+
+var i = 4;
+while(i > 0) {
+    result += 2;
+    i--;
+}
+
+console.log(result);
+
+console.log(rresult);
+var rresult = 7;
+function addTwo(a,b) {
+    return a
+}
+addTwo(5,10)
